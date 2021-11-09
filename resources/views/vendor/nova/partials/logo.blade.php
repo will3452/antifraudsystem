530a903869dc88140767e63cbc69e5420cc234c3
William Galas <william.galas@coreproc.ph>
2021-11-09 09:52:09 +0800

    Update logo.blade.php

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex justify-center">
     @if(!is_null(nova_get_setting('logo')))
-        <img src="/storage/{{nova_get_setting('logo')}}" alt="logo of the application" style="height: 100px;">
+        <img src="/storage/{{nova_get_setting('logo')}}" alt="logo of the application" style="height: 50px;">
     @else
         SYSTEM LOGO HERE
     @endif
